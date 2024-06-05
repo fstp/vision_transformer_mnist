e72a59a308b7fbff28453b53b6affea1ec53d4b1
John Blomberg <john.jb.blomberg@gmail.com>
2024-06-06 00:11:22 +0200

    Implement vision transformer

--- main.py
+++ main.py
@@ -50,6 +50,48 @@ class PatchEmbedding(nn.Module):
         return x
 
 
+class VisionTransformer(nn.Module):
+    def __init__(
+        self,
+        num_patches,
+        num_classes,
+        patch_size,
+        embed_dim,
+        num_encoders,
+        num_heads,
+        hidden_dim,
+        dropout,
+        activation,
+        in_channels,
+    ):
+        super().__init__()
+        self.embeddings_block = PatchEmbedding(
+            embed_dim, patch_size, num_patches, dropout, in_channels
+        )
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=embed_dim,
+            nhead=num_heads,
+            dropout=dropout,
+            activation=activation,
+            batch_first=True,
+            norm_first=True,
+        )
+        self.encoder_blocks = nn.TransformerEncoder(
+            encoder_layer=encoder_layer, num_layers=num_encoders
+        )
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(normalized_shape=embed_dim),
+            nn.Linear(in_features=embed_dim, out_features=num_classes),
+        )
+
+    def forward(self, x):
+        x = self.embeddings_block(x)
+        x = self.encoder_blocks(x)
+        x = x[:, 0, :]
+        x = self.mlp_head(x)
+        return x
+
+
 class TestPatchEmbedding(unittest.TestCase):
     def test_shape(self):
         model = PatchEmbedding(
@@ -59,9 +101,27 @@ class TestPatchEmbedding(unittest.TestCase):
         self.assertEqual(model(x).shape, (512, num_patches + 1, embed_dim))
 
 
+class TestVisionTransformer(unittest.TestCase):
+    def test_shape(self):
+        model = VisionTransformer(
+            num_patches,
+            num_classes,
+            patch_size,
+            embed_dim,
+            num_encoders,
+            num_heads,
+            hidden_dim,
+            dropout,
+            activation,
+            in_channels,
+        ).to(device)
+        x = torch.randn(512, 1, 28, 28)
+        self.assertEqual(model(x).shape, (512, num_classes))
+
+
 if __name__ == "__main__":
-    model = PatchEmbedding(embed_dim, patch_size, num_patches, dropout, in_channels).to(
-        device
-    )
-    ic(model)
+    # model = PatchEmbedding(embed_dim, patch_size, num_patches, dropout, in_channels).to(
+    #     device
+    # )
+    # ic(model)
     unittest.main()
