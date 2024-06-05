9bc89062149427be8eb04e1d552e9392f40c0a72
John Blomberg <john.jb.blomberg@gmail.com>
2024-06-05 23:45:46 +0200

    Patch embedding layer

--- main.py
+++ main.py
@@ -19,6 +19,40 @@ num_patches = (img_size // patch_size) ** 2  # 49
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, embed_dim, patch_size, num_patches, dropout, in_channels):
+        super().__init__()
+        self.patcher = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=embed_dim,
+                kernel_size=patch_size,
+                stride=patch_size,
+            ),
+            nn.Flatten(2),
+        )
+        self.cls_token = nn.Parameter(
+            torch.randn(size=(1, in_channels, embed_dim)), requires_grad=True
+        )
+        self.position_embeddings = nn.Parameter(
+            torch.randn(size=(1, num_patches + 1, embed_dim)), requires_grad=True
+        )
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        cls_token = self.cls_token.expand(x.shape[0], -1, -1)
+        x = self.patcher(x).permute(0, 2, 1)
+        x = torch.cat([cls_token, x], dim=1)
+        x = self.dropout(x)
+        return x
+
+
 if __name__ == "__main__":
-    test = torch.tensor([1, 2, 3])
-    ic(test)
+    model = PatchEmbedding(embed_dim, patch_size, num_patches, dropout, in_channels).to(
+        device
+    )
+    ic(model)
+    x = torch.randn(512, 1, 28, 28)
+    ic(model(x).shape)
+    ic(model(x).shape == (512, num_patches + 1, embed_dim))
